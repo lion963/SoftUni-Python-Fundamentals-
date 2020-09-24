ae9535b6506cf678c971661b5b15096e64e08f50
Asen Kamboshev <lion963@mail.bg>
2020-09-24 10:00:40 +0300

    tasks 24.09

---
+++ EXERCISE_DATA TYPES AND VARIABLES/Water Overflow.py
@@ -0,0 +1,12 @@
+n=int(input())
+
+capacity=255
+water=0
+
+for i in range(1,n+1):
+    line=int(input())
+    water+=line
+    if water>capacity:
+        print(f'Insufficient capacity!')
+        water-=line
+print(water)
