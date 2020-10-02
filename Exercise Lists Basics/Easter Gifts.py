7b001b892a89cf9d849763d3a205e38d6b37ca86
Asen Kamboshev <71500381+lion963@users.noreply.github.com>
2020-10-02 23:36:44 +0300

    correction 02.10

--- Exercise Lists Basics/Easter Gifts.py
+++ Exercise Lists Basics/Easter Gifts.py
@@ -7,23 +7,51 @@ while line!='No Money':
     line=input()
     if line=='No Money':
         break
-    line.split(' ')
+    line1=line.split(' ')
 
-    if 'OutofStock' in line:
-        for el in list1:
-            if el==line[1]:
-                el='None'
+    if 'OutOfStock' in line1:
+        for i in range(len(list1)):
+            if list1[i]==line1[1]:
+                list1.remove(list1[i])
+                list1.insert(i,'None')
 
-    if 'Required' in line:
-        for el in list1:
-            if el=='None':
-                list1.remove(el)
-        if int(line[2])>=len(list1)-1:
-            list1.pop(int(line[2]))
-            list1.insert(int(line[2]),line[1])
+    if 'Required' in line1:
+        if int(line1[2])<=len(list1)-1:
+            list1.pop(int(line1[2]))
+            list1.insert(int(line1[2]),line1[1])
 
-    if 'JustInCase' in line:
+    if 'JustInCase' in line1:
         list1.pop(len(list1)-1)
-        list1.append(line[1])
+        list1.append(line1[1])
 
-print(list1)
+for el in list1:
+    if el=='None':
+        list1.remove(el)
+
+str2=' '.join(list1)
+print(str2)
+
+# gifts = input().split(' ')
+#
+# command = input().split(' ')
+# while command[0] != 'No' and command[1] != 'Money':
+#     index = 0
+#     if command[0] == 'OutOfStock':
+#         gift = command[1]
+#         gifts = list(map(lambda lst: lst.replace(gift, "None"), gifts))
+#
+#     elif command[0] == 'Required':
+#         index = int(command[2])
+#         if 0 < index < len(gifts):
+#             gifts[index] = command[1]
+#
+#     elif command[0] == 'JustInCase':
+#         gifts[-1] = command[1]
+#
+#     command = input().split(' ')
+#
+# while 'None' in gifts:
+#     gifts.remove('None')
+#
+# for i in gifts:
+#     print(i, end=' ')
