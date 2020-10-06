abf27634df62d2267ff458e031fef7a6a464d110
Asen Kamboshev <lion963@mail.bg>
2020-10-06 13:12:54 +0300

    tasks 06.10

--- Exercise Functions/Smallest of Three Numbers.py
+++ Exercise Functions/Smallest of Three Numbers.py
@@ -3,3 +3,9 @@ def min_num(a=int(input()), b=int(input()), c=int(input())):
     small=min(a,b,c)
     print(small)
 min_num()
+
+# def min_num(a=int(input()), b=int(input()), c=int(input())):
+#     small=None
+#     small=min(a,b,c)
+#     return small
+# print(min_num())
