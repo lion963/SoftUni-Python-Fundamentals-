d0b5f40781407fa84c62e78e996244bdd206c186
Asen Kamboshev <lion963@mail.bg>
2020-10-22 16:31:12 +0300

    tasks 22.10

---
+++ Exercise Objects and Classes/Catalogue.py
@@ -0,0 +1,26 @@
+class Catalogue:
+    def __init__(self, name):
+        self.name = name
+        self.products = []
+
+    def add_product(self, product):
+        self.products.append(product)
+
+    def get_by_letter(self, first_letter):
+        list_by_letter = [product for product in self.products if product.find(first_letter) == 0]
+        return list_by_letter
+
+    def __repr__(self):
+        print(f'Items in the {catalogue.name} catalogue:')
+        self.products.sort()
+        return [print(product) for product in self.products]
+
+
+catalogue = Catalogue("Furniture")
+catalogue.add_product("Sofa")
+catalogue.add_product("Mirror")
+catalogue.add_product("Desk")
+catalogue.add_product("Chair")
+catalogue.add_product("Carpet")
+print(catalogue.get_by_letter("C"))
+print(catalogue)
