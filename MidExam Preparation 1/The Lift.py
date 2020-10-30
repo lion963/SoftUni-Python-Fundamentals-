19d5767052dcc7d30e5c4a48e94f1fc79389632e
Asen Kamboshev <lion963@mail.bg>
2020-10-30 14:30:48 +0200

    tasks 30.10

--- MidExam Preparation 1/The Lift.py
+++ MidExam Preparation 1/The Lift.py
@@ -25,3 +25,24 @@ elif available_spots > people_queue:
 
     print(f'The lift has empty spots!')
     print(' '.join(map(str, lift)))
+
+# people_waiting = int(input())
+# lift_state = list(map(int, input().split()))
+# wagons = []
+#
+# for lift in lift_state:
+#     while lift < 4 and people_waiting != 0:
+#         lift += 1
+#         people_waiting -= 1
+#     wagons.append(lift)
+#
+# wagons_to_str = [str(wagon) for wagon in wagons]
+#
+# if len(wagons) * 4 == sum(wagons) and people_waiting == 0:
+#     print(f"{' '.join(wagons_to_str)}")
+# elif len(wagons) * 4 > sum(wagons) and people_waiting == 0:
+#     print("The lift has empty spots!\n"
+#           f"{' '.join(wagons_to_str)}")
+# else:
+#     print(f"There isn't enough space! {people_waiting} people in a queue!\n"
+#           f"{' '.join(wagons_to_str)}")
