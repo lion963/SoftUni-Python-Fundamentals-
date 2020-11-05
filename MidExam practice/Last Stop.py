e9ca16997af516f810a689a7ff800e2bce80895d
Asen Kamboshev <lion963@mail.bg>
2020-11-05 09:53:46 +0200

    tasks 05.11

--- MidExam practice/Last Stop.py
+++ MidExam practice/Last Stop.py
@@ -14,17 +14,18 @@ while not command == 'END':
             list_of_painting.remove(paint_num)
     elif command.find('Switch') >= 0:
         word, paint_num1, paint_num2 = command.split()
-        if (paint_num1 and paint_num2) in list_of_painting:
+        if paint_num1 in list_of_painting and paint_num2 in list_of_painting:
             index_1 = list_of_painting.index(paint_num1)
             index_2 = list_of_painting.index(paint_num2)
             list_of_painting[index_1], list_of_painting[index_2] = list_of_painting[index_2], list_of_painting[
-                    index_1]
+                index_1]
     elif command.find('Insert') >= 0:
         word, place, paint_num = command.split()
         place = int(place)
-        if place+1 in range(len(list_of_painting)+1):
+        if place + 1 in range(len(list_of_painting) + 1):
             list_of_painting.insert(place + 1, paint_num)
     elif command.find('Reverse') >= 0:
-        list_of_painting=list_of_painting[::-1]
+        list_of_painting = list_of_painting[::-1]
     command = input()
+
 print(' '.join(list_of_painting))
