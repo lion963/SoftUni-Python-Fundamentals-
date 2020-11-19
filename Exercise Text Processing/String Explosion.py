d8edae14caad78b6d061aa47b0b66e4167784e87
Asen Kamboshev <lion963@mail.bg>
2020-11-19 14:09:50 +0200

    tasks 19.11

--- Exercise Text Processing/String Explosion.py
+++ Exercise Text Processing/String Explosion.py
@@ -1,34 +1,48 @@
-sequence = input()
+sequence = list(input())
 
-string = ''
-flag = True
-add = 0
+string = []
+bomb = 0
+for i in range(len(sequence)):
+    if bomb > 0 and sequence[i] != '>':
+        bomb -= 1
+    elif sequence[i] == '>':
+        string.append(sequence[i])
+        bomb += int(sequence[i + 1])
+    else:
+        string.append(sequence[i])
+print(''.join(string))
 
-while flag:
-
-    condit = False
-    count = 0
-    for i in range(len(sequence)):
-        if not sequence[i] == '>':
-            string += sequence[i]
-            if i == len(sequence) - 1:
-                flag = False
-                break
-        else:
-            string += sequence[i]
-            index = int(sequence[i + 1]) + add
-            for j in range(i + 1, i + index + 1):
-                if not sequence[j] == '>':
-                    count += 1
-                    add = index - count
-                else:
-                    sequence = sequence[j:]
-                    count = 0
-                    condit = True
-                    break
-        if count > 0:
-            sequence = sequence[j + 1:]
-            break
-        if condit:
-            break
-print(string)
+# sequence = input()
+#
+# string = ''
+# flag = True
+# add = 0
+#
+# while flag:
+#
+#     condit = False
+#     count = 0
+#     for i in range(len(sequence)):
+#         if not sequence[i] == '>':
+#             string += sequence[i]
+#             if i == len(sequence) - 1:
+#                 flag = False
+#                 break
+#         else:
+#             string += sequence[i]
+#             index = int(sequence[i + 1]) + add
+#             for j in range(i + 1, i + index + 1):
+#                 if not sequence[j] == '>':
+#                     count += 1
+#                     add = index - count
+#                 else:
+#                     sequence = sequence[j:]
+#                     count = 0
+#                     condit = True
+#                     break
+#         if count > 0:
+#             sequence = sequence[j + 1:]
+#             break
+#         if condit:
+#             break
+# print(string)
