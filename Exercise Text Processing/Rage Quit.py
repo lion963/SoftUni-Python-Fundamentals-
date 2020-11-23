2b1cd003563361a44f632089f4ec94f0a4d932a7
Asen Kamboshev <lion963@mail.bg>
2020-11-23 09:18:59 +0200

    tasks 23.11

--- Exercise Text Processing/Rage Quit.py
+++ Exercise Text Processing/Rage Quit.py
@@ -1,35 +1,59 @@
-symbols = input()
+line = input()
 
-symbols_dict = {}
-rage_message = ''
-key_str = ''
-unique_list = []
+current_result = ''
+result = ''
 
-while not symbols == '':
-    str1 = ''
-    number = ''
-    for i in range(len(symbols)):
-        if not symbols[i].isdigit():
-            str1 += symbols[i].upper()
-        else:
-            number += symbols[i]
-            if i == len(symbols) - 1:
-                number = int(number)
-                symbols = ''
-                break
-            elif not symbols[i + 1].isdigit():
-                number = int(number)
-                symbols = symbols[i + 1:]
-                break
-    symbols_dict[str1] = number
+index = 0
 
-for key, value in symbols_dict.items():
-    rage_message += key * value
-    key_str += key
+while index < len(line):
+    if not line[index].isdigit():
+        current_result += line[index]
+        index += 1
+    else:
+        number = ''
+        while index < len(line) and line[index].isdigit():
+            number += line[index]
+            index += 1
+        number = int(number)
+        current_result = current_result.upper() * number
+        result += current_result
+        current_result = ''
 
-for el in key_str:
-    if el not in unique_list and not el == ' ':
-        unique_list.append(el)
+print(f"Unique symbols used: {len(set(result))}")
+print(result)
 
-print(f'Unique symbols used: {len(unique_list)}')
-print(rage_message)
+# symbols = input()
+#
+# symbols_dict = {}
+# rage_message = ''
+# key_str = ''
+# unique_list = []
+#
+# while not symbols == '':
+#     str1 = ''
+#     number = ''
+#     for i in range(len(symbols)):
+#         if not symbols[i].isdigit():
+#             str1 += symbols[i].upper()
+#         else:
+#             number += symbols[i]
+#             if i == len(symbols) - 1:
+#                 number = int(number)
+#                 symbols = ''
+#                 break
+#             elif not symbols[i + 1].isdigit():
+#                 number = int(number)
+#                 symbols = symbols[i + 1:]
+#                 break
+#     symbols_dict[str1] = number
+#
+# for key, value in symbols_dict.items():
+#     rage_message += key * value
+#     key_str += key
+#
+# for el in key_str:
+#     if el not in unique_list and not el == ' ':
+#         unique_list.append(el)
+#
+# print(f'Unique symbols used: {len(unique_list)}')
+# print(rage_message)
--- Exercise Text Processing/Winning Ticket.py
+++ Exercise Text Processing/Winning Ticket.py
@@ -1,35 +1,76 @@
-tickets_list = input().split(', ')
+tickets = input().split(", ")
+winning_symbols = ['@', '$', '#', '^']
 
-for ticket in tickets_list:
-    ticket = ticket.strip()
+
+def is_jackpot(ticket):
+    left_side = ticket[0:10]
+    right_side = ticket[10:20]
+    if not left_side == right_side:
+        return False
+    for winning_symbol in winning_symbols:
+        if winning_symbol * 10 == left_side:
+            print(f'ticket "{ticket}" - 10{winning_symbol} Jackpot!')
+            return True
+    return False
+
+
+def is_winning_ticket(ticket):
+    left_side = ticket[0:10]
+    right_side = ticket[10:20]
+    for winning_symbol in winning_symbols:
+        if winning_symbol * 6 in left_side and winning_symbol * 6 in right_side:
+            count_left = left_side.count(winning_symbol)
+            count_right = right_side.count(winning_symbol)
+            count = min(count_left, count_right)
+            print(f'ticket "{ticket}" - {count}{winning_symbol}')
+            return True
+    return False
+
+
+for t in tickets:
+    ticket = t.strip()
     if not len(ticket) == 20:
-        print(f'invalid ticket')
-    else:
-        left_ticket = ticket[:10]
-        right_ticket = ticket[10:]
-        if 6 <= left_ticket.count('@') < 10 and 6 <= right_ticket.count('@') < 10:
-            match_length = left_ticket.count('@')
-            print(f'ticket \"{ticket}\" - {match_length}@')
-        elif 6 <= left_ticket.count('#') < 10 and 6 <= right_ticket.count('#') < 10:
-            match_length = left_ticket.count('#')
-            print(f'ticket \"{ticket}\" - {match_length}#')
-        elif 6 <= left_ticket.count('$') < 10 and 6 <= right_ticket.count('$') < 10:
-            match_length = left_ticket.count('$')
-            print(f'ticket \"{ticket}\" - {match_length}$')
-        elif 6 <= left_ticket.count('^') < 10 and 6 <= right_ticket.count('^') < 10:
-            match_length = left_ticket.count('^')
-            print(f'ticket \"{ticket}\" - {match_length}^')
-        elif ticket.count('@') == 20:
-            match_length = ticket.count('@') // 2
-            print(f'ticket \"{ticket}\" - {match_length}@ Jackpot!')
-        elif ticket.count('#') == 20:
-            match_length = ticket.count('#') // 2
-            print(f'ticket \"{ticket}\" - {match_length}# Jackpot!')
-        elif ticket.count('$') == 20:
-            match_length = ticket.count('$') // 2
-            print(f'ticket \"{ticket}\" - {match_length}$ Jackpot!')
-        elif ticket.count('^') == 20:
-            match_length = ticket.count('^') // 2
-            print(f'ticket \"{ticket}\" - {match_length}^ Jackpot!')
-        else:
-            print(f'ticket \"{ticket}\" - no match')
+        print("invalid ticket")
+        continue
+    if is_jackpot(ticket):
+        continue
+    if is_winning_ticket(ticket):
+        continue
+
+    print(f'ticket "{ticket}" - no match')
+
+# tickets_list = input().split(', ')
+#
+# for ticket in tickets_list:
+#     ticket = ticket.strip()
+#     if not len(ticket) == 20:
+#         print(f'invalid ticket')
+#     else:
+#         left_ticket = ticket[:10]
+#         right_ticket = ticket[10:]
+#         if 6 <= left_ticket.count('@') < 10 and 6 <= right_ticket.count('@') < 10:
+#             match_length = left_ticket.count('@')
+#             print(f'ticket \"{ticket}\" - {match_length}@')
+#         elif 6 <= left_ticket.count('#') < 10 and 6 <= right_ticket.count('#') < 10:
+#             match_length = left_ticket.count('#')
+#             print(f'ticket \"{ticket}\" - {match_length}#')
+#         elif 6 <= left_ticket.count('$') < 10 and 6 <= right_ticket.count('$') < 10:
+#             match_length = left_ticket.count('$')
+#             print(f'ticket \"{ticket}\" - {match_length}$')
+#         elif 6 <= left_ticket.count('^') < 10 and 6 <= right_ticket.count('^') < 10:
+#             match_length = left_ticket.count('^')
+#             print(f'ticket \"{ticket}\" - {match_length}^')
+#         elif ticket.count('@') == 20:
+#             match_length = ticket.count('@') // 2
+#             print(f'ticket \"{ticket}\" - {match_length}@ Jackpot!')
+#         elif ticket.count('#') == 20:
+#             match_length = ticket.count('#') // 2
+#             print(f'ticket \"{ticket}\" - {match_length}# Jackpot!')
+#         elif ticket.count('$') == 20:
+#             match_length = ticket.count('$') // 2
+#             print(f'ticket \"{ticket}\" - {match_length}$ Jackpot!')
+#         elif ticket.count('^') == 20:
+#             match_length = ticket.count('^') // 2
+#             print(f'ticket \"{ticket}\" - {match_length}^ Jackpot!')
+#         else:
+#             print(f'ticket \"{ticket}\" - no match')
