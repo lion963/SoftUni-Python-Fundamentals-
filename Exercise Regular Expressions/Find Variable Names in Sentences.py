0dc6e835e893fe59372ad2056f0c7ca4d721b99e
Asen Kamboshev <lion963@mail.bg>
2020-11-25 15:57:14 +0200

    tasks 25.11

--- Exercise Regular Expressions/Find Variable Names in Sentences.py
+++ Exercise Regular Expressions/Find Variable Names in Sentences.py
@@ -2,7 +2,7 @@ import re
 
 list1 = input().split()
 result = []
-pattern = '^[_][0-9A-Za-z]*[0-9A-Za-z]$'
+pattern = r'^[_][0-9A-Za-z]*[0-9A-Za-z]$'
 
 for el in list1:
     result += re.findall(pattern, el)
