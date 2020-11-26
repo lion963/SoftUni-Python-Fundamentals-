95e1c1329514a5d9efdcf359c1a1ffbef9708a1b
Asen Kamboshev <lion963@mail.bg>
2020-11-26 11:07:07 +0200

    tasks 26.11

--- Exercise Regular Expressions/Extract Emails.py
+++ Exercise Regular Expressions/Extract Emails.py
@@ -1,6 +1,6 @@
 import re
 
 text = input()
-pattern = r"(?<=^|(?<=\s))([A-Za-z0-9]+[._-]?[A-Za-z0-9]+@[A-Za-z]+[.-]?[A-Za-z]+[.]{1}[A-Za-z]+)(?=.$|(?=\s))"
+pattern = r"(?<=^|(?<=\s))([A-Za-z0-9][\w.-]+?[A-Za-z0-9]@[A-Za-z][A-Za-z.-]+?[A-Za-z].[A-Za-z]+)(?=.$|(?=\s))"
 result = re.findall(pattern, text)
 print('\n'.join(result))
