6229f6e878706c1824d06d4b7ffb54813dc60ece
Asen Kamboshev <lion963@mail.bg>
2020-11-26 10:40:07 +0200

    tasks 26.11

--- Exercise Regular Expressions/Extract Emails.py
+++ Exercise Regular Expressions/Extract Emails.py
@@ -1,7 +1,6 @@
 import re
 
-pattern = '[a-zA-Z0-9][\w.-]*[^_\W][@][a-zA-Z0-9][\w.-]*[a-zA-Z][.][a-zA-Z]*[^.]'
-
 text = input()
+pattern = r"(?<=^|(?<=\s))([A-Za-z0-9]+[._-]?[A-Za-z0-9]+@[A-Za-z]+[.-]?[A-Za-z]+[.]{1}[A-Za-z]+)(?=.$|(?=\s))"
 result = re.findall(pattern, text)
 print('\n'.join(result))
