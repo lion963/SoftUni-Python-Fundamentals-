e9bd1cc07c9c468cee946996014293b4b8433178
Asen Kamboshev <lion963@mail.bg>
2020-11-26 09:33:42 +0200

    tasks 26.11

--- Exercise Regular Expressions/Find Occurrences of Word in Sentence.py
+++ Exercise Regular Expressions/Find Occurrences of Word in Sentence.py
@@ -3,6 +3,6 @@ import re
 sentence = input()
 word = input()
 
-pattern='(\W|^|(?<=\s))' + word + '((?=\s)|\W)'
+pattern='(\W|^|(?<=\s))' + word + '((?=\s)|\W|$)'
 result = re.findall(pattern, sentence, re.IGNORECASE)
 print(len(result))
