09e5dc12116d9960c7728a2b58170a3395372d87
Asen Kamboshev <lion963@mail.bg>
2020-11-30 12:33:36 +0200

    tasks 30.11

--- Exercise Regular Expressions/Extract the Links.py
+++ Exercise Regular Expressions/Extract the Links.py
@@ -1,16 +1,32 @@
 import re
-from operator import itemgetter
 
 web_list = []
 pattern = r'([w]{3}.[A-Za-z0-9-]+(\.[a-z]+)+)'
 while True:
     line = input()
     if line:
-        web = re.findall(pattern, line)
-        if web:
-            web_list.append(list(map(itemgetter(0), web))[0])
+        for el in re.finditer(pattern, line):
+            web_list.append(el.group())
     else:
         break
 
 for el in web_list:
     print(el)
+
+
+# import re
+# from operator import itemgetter
+#
+# web_list = []
+# pattern = r'([w]{3}.[A-Za-z0-9-]+(\.[a-z]+)+)'
+# while True:
+#     line = input()
+#     if line:
+#         web = re.findall(pattern, line)
+#         if web:
+#             web_list.append(list(map(itemgetter(0), web))[0])
+#     else:
+#         break
+#
+# for el in web_list:
+#     print(el)
